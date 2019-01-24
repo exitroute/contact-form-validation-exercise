b441d9d74b4724486540c35195267cf06ee16d5b
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-01-24 16:29:35 +0100

    state, handlers and validators added to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,61 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      nameControl: null
+    }
+  };
+
+  // addContactData = (data) => {
+  //     console.log(data);
+
+  //   this.setState((state) => {
+  //     state = data;
+  //     return state;
+  //   });
+  // }
+
+  handleNameInputChange = (e) => {
+    console.log(this.nameInput.current.value)
+    if (this.nameInput.current.value.length > 5) {
+      this.setState({ nameControl: false })
+    } else {
+      this.setState({ nameControl: true })
+    }
+    // this.addContactData(input);    
+  }
+
+  validateEmail = (email) => {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  handleMailInputChange = (e) => {
+    let email = this.mailInput.current.value;
+    if (this.validateEmail(email)) {
+      this.setState({ email: false })
+    } else {
+      this.setState({ email: true })
+    }
+    // this.addContactData(input);
+  }
+
+  // displayControl = () => {
+
+  //   console.log(this.state.name);
+
+  //   if (this.state.nameControl === null) { let nameInputClass = "form-control"; return nameInputClass }
+  //   else if (this.state.nameControl === true) { let nameInputClass = "is-valid"; return nameInputClass }
+  //   else if (this.state.nameControl === false) { let nameInputClass = "is-invalid"; return nameInputClass };
+
+  // }
+
+  nameInput = React.createRef();
+  mailInput = React.createRef();
+
   render() {
     return (
       <div className="container">
@@ -14,27 +69,30 @@ class App extends Component {
             <label htmlFor="contactName">Name</label>
             <input
               type="text"
-              className="form-control"
+              className={
+                this.state.nameControl === null
+                  ? "form-control" : this.state.nameControl === true
+                    ? "form-control is-valid" : "form-control is-invalid"
+              }
               id="contactName"
-              placeholder="Enter name" />
+              placeholder="Enter name"
+              ref={this.nameInput}
+              onChange={this.handleNameInputChange} />
             <small
               id="contactName"
               className="form-text text-muted">
               Never add a contact with no potential.</small>
           </div>
           <div className="form-group">
-            <label htmlFor="contactPhone">The Phone Number</label>
+            <label htmlFor="contactEmail">Email</label>
             <input
-              type="tel"
-              className="form-control"
-              id="contactPhone"
-              placeholder="01294 8331932" />
+              type="email"
+              className={this.state.email ? " form-control is-invalid" : " form-control is-valid"}
+              id="contactEmail"
+              placeholder="contact-name@mailserver.com"
+              ref={this.mailInput}
+              onChange={this.handleMailInputChange} />
           </div>
-          {/* <button
-            type="submit"
-            className="btn btn-primary">
-            Submit
-          </button> */}
         </form>
       </div>
     );
